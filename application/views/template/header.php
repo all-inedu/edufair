e8f8b5713560dd4ccd5cfc1fd9698d2fbaca1dcf
ManuelEric <manuel.eric.b@gmail.com>
2021-06-09 11:23:20 +0700

    changes

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -5,13 +5,13 @@
 
 <head>
     <title><?=$title;?></title>
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css"
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <link href="<?php echo base_url(); ?>assets/css/slimselect.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/all.min.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/animate.min.css" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/slim-select/1.27.0/slimselect.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link rel="stylesheet" href="<?=base_url('assets/css/flipTimerss.css');?>" />
-    <link href="<?php echo base_url(); ?>assets/css/aos.css" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <style type="text/css">
     @font-face { font-family: 'SF-Pro-Display-Regular'; src: url('<?php echo base_url(); ?>assets/font/SF-Pro-Display-Regular.otf');  }
     @font-face { font-family: 'Montserrat-ExtraBold'; src: url('<?php echo base_url(); ?>assets/font/Montserrat-ExtraBold.otf');  }
