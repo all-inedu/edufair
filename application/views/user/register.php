5bdfc0fcf7c3cc1baaf27e2349790e9261b144f7
unknown <manuel.eric.b@gmail.com>
2022-05-23 18:18:15 +0700

    minor update

--- application/views/user/register.php
+++ application/views/user/register.php
@@ -11,7 +11,7 @@ body {
         <h3 class="text-center" style="letter-spacing: 0.2em;margin-bottom: 1em;">REGISTRATION</h3>
         <hr>
         <form method="get" id="registerForm" novalidate class="needs-validation">
-            <input type="hidden" name="param" value="<?php isset($_GET['param']) ? $_GET['param'] : ''?>">
+            <input type="hidden" name="param" value="<?php echo isset($_GET['param']) ? $_GET['param'] : ''?>">
             <div class="row mt-4">
                 <div class="col" data-page="1">
                     <div class="row">
